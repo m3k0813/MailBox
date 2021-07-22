9727c95bf103216bd61712c828f43838c6e112a5
m3k0813 <m3k0813@gmail.com>
2021-07-22 21:41:23 +0900

    Naver Mail

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from selenium import webdriver
 import pymysql
 import json
 import requests
-
+from selenium.webdriver.common.keys import Keys
+import pyperclip
+import time
 db = pymysql.connect(
     user='root',
     passwd='',
@@ -34,7 +36,7 @@ def Kakao(text):
 
 
 def Daum():
-    driver = webdriver.Chrome('./chromedriver.exe')
+    driver = webdriver.Chrome('/chromedriver.exe')
     driver.implicitly_wait(2)
     driver.get(
         'https://logins.daum.net/accounts/signinform.do?url=https%3A%2F%2Fmail.daum.net%2F')
@@ -49,27 +51,73 @@ def Daum():
     driver.find_element_by_xpath('//*[@id="loginBtn"]').click()
 
     # 메일 제목 추출
-    titles = driver.find_element_by_css_selector('strong.tit_subject')
+    title = driver.find_element_by_css_selector('strong.tit_subject')
     # 메일 날짜 추출
-    dates = driver.find_element_by_class_name('txt_date')
+    date = driver.find_element_by_class_name('txt_date')
 
     curs = db.cursor(pymysql.cursors.DictCursor)
 
     curs.execute('SELECT * FROM dates;')  # Select 데이터 조회
     date_db = curs.fetchone()
 
-    if(date_db['date'] == dates.text):
-        Kakao("새 메일이 없습니다.")
-    else:
-        Kakao(titles.text)
+    if(date_db['date'] != date.text):
+        Kakao("새 메일: "+title.text)
 
         sql = "update dates set date = %s"  # 데이터 수정
-        val = (dates.text)
+        val = (date.text)
         curs.execute(sql, val)
         db.commit()
+        db.close()
+        driver.quit()
 
+
+def Naver():
+    driver = webdriver.Chrome('chromedriver.exe')
+    driver.get(
+        'https://nid.naver.com/nidlogin.login?url=http%3A%2F%2Fmail.naver.com%2F')
+
+    # 아이디 입력폼
+    tag_id = driver.find_element_by_name('id')
+    # 패스워드 입력폼
+    tag_pw = driver.find_element_by_name('pw')
+
+    my_id = ''
+    my_pwd = ''
+
+    tag_id.click()
+    pyperclip.copy(my_id)
+    tag_id.send_keys(Keys.CONTROL, 'v')  # 복사 후 붙혀넣기
+
+    tag_pw.click()
+    pyperclip.copy(my_pwd)
+    tag_pw.send_keys(Keys.CONTROL, 'v')
+
+    # 로그인 버튼 클릭
+    driver.find_element_by_id('log.login').click()
+
+    # 네이버 기기 등록
+    driver.find_element_by_id('new.save').click()
+    time.sleep(3)
+
+    # 메일 제목
+    title = driver.find_element_by_css_selector("strong.mail_title")
+    index = title.get_attribute("mailsn")  # 메일 고유 번호
+
+    curs = db.cursor(pymysql.cursors.DictCursor)
+
+    curs.execute('SELECT * FROM num;')  # Select 데이터 조회
+    num_db = curs.fetchone()
+
+    if(num_db['mailsn'] != index):
+        Kakao("새 메일: "+title.text)
+        sql = "update num set mailsn = %s"  # 데이터 수정
+        val = (index)
+        curs.execute(sql, val)
+        db.commit()
+        db.close()
         driver.quit()
 
 
 if __name__ == "__main__":
-    Daum()
+    Daum()         # 당일 메세지 시분 표시 > 고유 값 찾아야됨
+    Naver()
